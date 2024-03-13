7fdb07f8c629ec6622051c1e39a59f99ebf237a9
Alex_Kogut13 <akogut@engenious.io>
2024-03-13 16:31:48 +0200

    Initial Commit

---
+++ ListView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ListView
+//
+//  Created by Alex Kogut on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by Alex Kogut on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
